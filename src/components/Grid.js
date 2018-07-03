db094276c5cea3b96ff86d1d4cbcb239b706a894
usama54321 <usama54321@gmail.com>
2018-07-03 15:48:01 +0500

    Moving grid to separate file

---
+++ src/components/Grid.js
@@ -0,0 +1,62 @@
+export default class Grid extends Phaser.Group {
+    constructor(cols = 3, rows = 3, cw = 100) {
+        super(game);
+        //promote to a class variable
+        this.cellWidth = cw
+        this.rows = rows
+        this.cols = cols
+        this.objects = {}
+    }
+
+    placeAt(xx,yy,obj) {
+    	//Calculate target coordinates
+    	var x2 = this.cellWidth * xx + this.cellWidth / 2;
+        var y2 = this.cellWidth * yy + this.cellWidth / 2;
+        obj.x = x2;
+        obj.y = y2;
+
+    }
+
+    moveObject(xx,yy, obj) {
+	  //xx is number of cells to move right
+	  //yy is number of cells to move down
+
+      var pixels //Number of pixels to move
+
+      var speed = 100
+      var direction //Direction in which to move
+
+      //Direction is positive or negative depending on sign of xx and yy 
+      direction = xx > 0 ? 1 : -1
+      pixels = xx*this.cellWidth
+      //Object is given a starting constant velocity
+      obj.body.velocity.x = speed * direction
+
+      //Reset velocity to zero when target cell is reached
+      setTimeout(function(){ obj.body.velocity.x=0 }, (pixels/speed)*1000);
+
+      //Repeated for downward movement
+      direction = yy > 0 ? 1 : -1
+      pixels = yy*this.cellWidth
+      obj.body.velocity.y = speed * direction
+
+      setTimeout(function(){ obj.body.velocity.y=0 }, (pixels/speed)*1000);  
+    }
+
+
+    //mostly for planning and debugging this will
+    //create a visual representation of the grid
+    show() {
+        this.graphics = game.add.graphics();
+        this.graphics.lineStyle(1, 0x000000, 1);
+
+        for (var i = 0; i <= this.rows*this.cellWidth; i += this.cellWidth) {
+            this.graphics.moveTo(i, 0);
+            this.graphics.lineTo(i, this.cols*this.cellWidth);
+        }
+        for (var i = 0; i <= this.cols*this.cellWidth; i += this.cellWidth) {
+            this.graphics.moveTo(0, i);
+            this.graphics.lineTo(this.rows*this.cellWidth, i);
+        }
+    }
+}
--- src/states/Game.js
+++ src/states/Game.js
@@ -7,6 +7,7 @@ import Eval from '../services/Eval'
 import { compile } from '@sweet-js/core/dist/browser-sweet'
 import NodeLoader from '@sweet-js/core/dist/node-loader'
 import Urdu from '../services/Urdu.sjs'
+import Grid from '../components/Grid'
 
 export default class extends Phaser.State {
   init() { 
@@ -39,7 +40,7 @@ export default class extends Phaser.State {
 
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
-    var grid = new AlignGrid(6, 6, 100)
+    var grid = new Grid(6, 6, 100)
     grid.show()
 
     //Initialize sprite
@@ -85,66 +86,4 @@ export default class extends Phaser.State {
             document.querySelector('#answer').innerHTML += data.answer
     })
   }
-}
-
-class AlignGrid extends Phaser.Group {
-    constructor(cols = 3, rows = 3, cw = 100) {
-        super(game);
-        //promote to a class variable
-        this.cellWidth = cw
-        this.rows = rows
-        this.cols = cols
-    }
-
-    placeAt(xx,yy,obj) {
-    	//Calculate target coordinates
-    	var x2 = this.cellWidth * xx + this.cellWidth / 2;
-        var y2 = this.cellWidth * yy + this.cellWidth / 2;
-        obj.x = x2;
-        obj.y = y2;
-
-    }
-
-    moveObject(xx,yy, obj) {
-	  //xx is number of cells to move right
-	  //yy is number of cells to move down
-
-      var pixels //Number of pixels to move
-
-      var speed = 100
-      var direction //Direction in which to move
-
-      //Direction is positive or negative depending on sign of xx and yy 
-      direction = xx > 0 ? 1 : -1
-      pixels = xx*this.cellWidth
-      //Object is given a starting constant velocity
-      obj.body.velocity.x = speed * direction
-
-      //Reset velocity to zero when target cell is reached
-      setTimeout(function(){ obj.body.velocity.x=0 }, (pixels/speed)*1000);
-
-      //Repeated for downward movement
-      direction = yy > 0 ? 1 : -1
-      pixels = yy*this.cellWidth
-      obj.body.velocity.y = speed * direction
-
-      setTimeout(function(){ obj.body.velocity.y=0 }, (pixels/speed)*1000);  
-    }
-
-
-    //mostly for planning and debugging this will
-    //create a visual representation of the grid
-    show() {
-        this.graphics = game.add.graphics();
-        this.graphics.lineStyle(1, 0x000000, 1);
-
-        for (var i = 0; i <= this.rows*this.cellWidth; i += this.cellWidth) {
-            this.graphics.moveTo(i, 0);
-            this.graphics.lineTo(i, this.cols*this.cellWidth);
-        }
-        for (var i = 0; i <= this.cols*this.cellWidth; i += this.cellWidth) {
-            this.graphics.moveTo(0, i);
-            this.graphics.lineTo(this.rows*this.cellWidth, i);
-        }
-    }
 }
