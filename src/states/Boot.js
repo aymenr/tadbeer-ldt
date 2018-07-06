1c1db4e4c8c5adf4a0918bc69c8aaf6ac879edfd
Mohammad Maaz <mohammadmaaz1997@gmail.com>
2018-07-06 15:30:16 +0500

    Isometric grid of assets with movement function

--- src/states/Boot.js
+++ src/states/Boot.js
@@ -9,6 +9,15 @@ export default class extends Phaser.State {
     FBInstant.setLoadingProgress(50);
     //load any assets here
     this.load.image('dude', 'assets/images/phaser-dude.png')
+    this.load.image('road1', 'assets/images/tiles/road1.png')
+    this.load.image('road2', 'assets/images/tiles/road2.png')
+    this.load.image('grass', 'assets/images/tiles/grass.png')
+    this.load.image('water', 'assets/images/tiles/water.png')
+    this.load.image('water', 'assets/images/tiles/sewage_open.png')
+    this.load.image('rickshaw1', 'assets/images/rickshaw1.png')
+    this.load.image('rickshaw2', 'assets/images/rickshaw2.png')
+    this.load.image('wall1', 'assets/images/wall1.png')
+    this.load.image('wall2', 'assets/images/wall2.png')
 
     FBInstant.setLoadingProgress(100);
   }
--- src/states/Game.js
+++ src/states/Game.js
@@ -7,7 +7,7 @@ import Eval from '../services/Eval'
 import { compile } from '@sweet-js/core/dist/browser-sweet'
 import NodeLoader from '@sweet-js/core/dist/node-loader'
 import Urdu from '../services/Urdu.sjs'
-import Grid from '../components/Grid'
+import Grid from '../components/IsoGrid'
 
 export default class extends Phaser.State {
   init() { 
@@ -38,23 +38,29 @@ export default class extends Phaser.State {
     let board = document.getElementById('content')
    	container.appendChild(board)
 
+
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
-    var grid = new Grid(6, 6, 100)
-    grid.show()
+    var grid = new Grid(6, 6, 'grass')
 
-    //Initialize sprite
-    var player = game.add.sprite(0, 0, 'dude')
-    game.physics.arcade.enable(player);
-    player.anchor.setTo(0.5, 0.5);
-    player.body.collideWorldBounds = true;
-    player.body.bounce.setTo(0, 0);
+    grid.modify(4,3,'water')
+    grid.modify(4,4,'water')
+    for (var i=0; i<6; i++){
+      grid.modify(1,i,'road1')
+    }
+
+    grid.display()
+
+    // //Initialize sprite
+    var rickshaw = game.add.sprite(0, 0, 'rickshaw1')
+
+    var wall = game.add.sprite(0, 0, 'wall2')
 
     //Place at particular coordinates
-   	grid.placeAt(0,0,player)
-
-   	//Move a certain number of paces along the grid
-    grid.moveObject(3,2,player)
+   	grid.placeObject(1,0,rickshaw)
+    grid.placeObject(4,5, wall)
+   	// //Move a certain number of paces along the grid
+    grid.moveObject(1,4,rickshaw)
 
 
 
@@ -86,4 +92,25 @@ export default class extends Phaser.State {
             document.querySelector('#answer').innerHTML += data.answer
     })
   }
+}
+
+function cartesianToIsometric(cartPt){
+    var tempPt=new Phaser.Point();
+    tempPt.x=cartPt.x-cartPt.y;
+    tempPt.y=(cartPt.x+cartPt.y)/2;
+    return (tempPt);
+}
+
+function isometricToCartesian(isoPt){
+    var tempPt=new Phaser.Point();
+    tempPt.x=(2*isoPt.y+isoPt.x)/2;
+    tempPt.y=(2*isoPt.y-isoPt.x)/2;
+    return (tempPt);
+}
+
+function getTileCoordinates(cartPt, tileHeight){
+    var tempPt=new Phaser.Point();
+    tempPt.x=Math.floor(cartPt.x/tileHeight);
+    tempPt.y=Math.floor(cartPt.y/tileHeight);
+    return(tempPt);
 }
