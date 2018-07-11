9648eb18dfab058bf43278af5e7be774edf80dc0
Mohammad Maaz <mohammadmaaz1997@gmail.com>
2018-07-11 11:13:02 +0500

    Added scaling of sprites for different screen sizes

--- src/states/Game.js
+++ src/states/Game.js
@@ -7,7 +7,7 @@ import Eval from '../services/Eval'
 import { compile } from '@sweet-js/core/dist/browser-sweet'
 import NodeLoader from '@sweet-js/core/dist/node-loader'
 import Urdu from '../services/Urdu.sjs'
-import Grid from '../components/IsoGrid'
+import Grid from '../components/Grid'
 
 export default class extends Phaser.State {
   init() { 
@@ -41,26 +41,27 @@ export default class extends Phaser.State {
 
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
-    var grid = new Grid(6, 6, 'grass')
+    var grid = new Grid(6, 6)
 
-    grid.modify(4,3,'water')
-    grid.modify(4,4,'water')
-    for (var i=0; i<6; i++){
-      grid.modify(1,i,'road1')
-    }
+    var gameBoard = [
+        ['grass', 'grass', 'grass', 'grass', 'grass', 'grass'],
+        ['grass', 'grass', 'grass', 'grass', 'grass', 'grass'],
+        ['road1', 'road1', 'road1', 'road1', 'road1', 'road1'],
+        ['grass', 'grass', 'grass', 'grass', 'grass', 'grass'],
+        ['grass', 'grass', 'grass', 'grass', 'grass', 'grass'],
+        ['grass', 'grass', 'grass', 'grass', 'grass', 'grass'],
+      ];
 
-    grid.display()
+    //grid.display()
+
+    grid.render(gameBoard)
 
     // //Initialize sprite
     var rickshaw = game.add.sprite(0, 0, 'rickshaw1')
+   	grid.placeObject(2,0,rickshaw)
 
-    var wall = game.add.sprite(0, 0, 'wall2')
-
-    //Place at particular coordinates
-   	grid.placeObject(1,0,rickshaw)
-    grid.placeObject(4,5, wall)
-   	// //Move a certain number of paces along the grid
-    grid.moveObject(1,4,rickshaw)
+    //Move a certain number of paces along the grid, relative to current position
+    grid.moveObject(0,5,rickshaw)
 
 
 
